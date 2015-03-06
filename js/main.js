6bbdb01e134b43aa0eb4f5af9fbb3931d5067bc4
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-06 16:54:38 -0500

    completed the bulk of functionality with lots of help

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var EmployeeCollection = Backbone.Collection.extend({ // make a collection that 
 
 //--------------------------------------------------Filtered Employee Collection----------------//
 
-var FilteredEmployeeCollection = EmployeeCollection.extend({
+var FilteredEmployeeCollection = EmployeeCollection.extend({ //empty bucket to hold filtered selection
 
 });
 
@@ -30,34 +30,16 @@ var EmployeeView = Backbone.View.extend({
 
 });
 
-var CheckboxView = Backbone.View.extend({
-  template: JST["dept"],
-  tagname: "li",
-
-  render: function() {
-    var markup = this.template( this.model.toJSON() );
-    this.$el.html(markup);
-
-    return this;
-  }
-
-});
 
 
 //--------------------------------------------------Glue Code----------------//
-
-// 4. `function you create`
- // 5. goes through your list of departments and sees whats checked
- // 6. filters the EmployeeCollection
- // 7. sets those results on the FilteredEmployeesCollection
- // WIN.
 
 function formatTimes() {
   _.each($("time[datetime]"), function(e) {
   var $el = $(e); 
   $el.text(moment($el.attr('datetime'))
   .format("MMMM Do, YYYY")) });
-}
+};
 
 $(function(){ 
 
@@ -65,56 +47,60 @@ $(function(){
   var filtered = new FilteredEmployeeCollection();//filtered employees
 
 
-  employees.on("add", function(model) {
+  filtered.on("add", function(model) {
     var employeeView = new EmployeeView({model: model});
-
-
+    employeeView.listenTo(model, 'remove', employeeView.remove);
     $(".table-body").append(employeeView.render().el);
-  });
+  })
 
  employees.fetch().done(function(){
 
     var firstModel = employees.first();
-      var headings = firstModel.keys();
-      //console.log(headings);
-      _.each(headings, function(heading) {
-         $("thead tr").append($("<th />").text(heading)); // appends heading in the employee view
-      });
-
-      formatTimes();
-
-
-      _.uniq(employees.map(function(e) { 
-        console.log(e.attributes.Dept);
-        return e.attributes.Dept; 
-        _.each()
-      }));
-  
-
-    // 1. map employee departments, uniq X
-   // 2. for each department, create the filter checkbox (probably its own template)
-  // 3. it'll handle an on change event that calls a `function you create`
-
-   // for each employee, 
-  // format the date
-     // set the FormattedStart attribute to the formatted date.
-
-     /*var CheckboxView = Backbone.View.extend({
-        template: JST["dept"],
-        tagname: "li",
-        var department = new EmployeeCollection();
-
-      getRoles: function() {
-        return _.uniq(this.department.pluck("dept"), false, function(dept){
-        return dept;
-        console.log(dept); 
+    var headings = firstModel.keys();
+    _.each(headings, function(heading) {
+       $("thead tr").append($("<th />").text(heading)); // appends heading in the employee view
     });
-  }
-});*/
 
-  });  //end fetch
+    var checkboxes = _.uniq(employees.map(function(e) {  //maps dept attributes
 
+      return e.attributes.Dept; 
+
+    }));
+
+    _.each(checkboxes, function(dept) {             //set checkbox to list
+      var markup = JST["dept"]({ name: dept });
+      $("ul").append($("<li />").html(markup));
+    });
+  
+    checkAllFilters();
+  });//end fetch
+
+  $(document).on('change', 'input.dept',function () {
+    checkAllFilters();
+  })
+
+  function checkAllFilters() {
+    var checked = _.map($('input.dept:checked'), function(e) { 
+      return $(e).attr('name'); 
+    })
+    console.log(checked);
+
+    if (checked.length === 0) {
+      filtered.set(employees.models);
+    } else {
+      var myChoice = employees.filter(function(employee) {  
+        return _.contains(checked, employee.get('Dept')); 
+      });
+      filtered.set(myChoice);
+    };
+
+    formatTimes();
+  };
 });
+
+
+
+
 
 
 
