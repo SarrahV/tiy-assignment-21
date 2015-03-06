0acd879cded7cf60b4adf0533b49f1e67e156579
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-06 11:34:40 -0500

    got moment working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 
 var Employee = Backbone.Model.extend(); // make an Employee Model
 
+//--------------------------------------------------Employee Collection----------------//
+
 var EmployeeCollection = Backbone.Collection.extend({ // make a collection that uses the employee model
     url: "data.json",
     model: Employee
 });
 
+//--------------------------------------------------Filtered Employee Collection----------------//
+
+var FilteredEmployeeCollection = EmployeeCollection.extend({
+
+});
+
+
+//--------------------------------------------------Employee View----------------//
 
 var EmployeeView = Backbone.View.extend({
   template: JST["employeeRow"],
@@ -16,44 +26,95 @@ var EmployeeView = Backbone.View.extend({
     this.$el.html(markup);
 
     return this;
-  },
+  }
 
 });
 
-/*var CheckboxView = Backbone.View.extend({
-  template: JST["roles"],
+var CheckboxView = Backbone.View.extend({
+  template: JST["dept"],
   tagname: "li",
-  var employeeRole = new EmployeeCollection();
 
-  getRoles: function() {
-    return _.uniq(this.employeeRole.pluck("role"), false, function(role){
-      return role;
-      console.log(role);
-    });
+  render: function() {
+    var markup = this.template( this.model.toJSON() );
+    this.$el.html(markup);
+
+    return this;
   }
 
-});*/
+});
 
-$(function(){
+
+//--------------------------------------------------Glue Code----------------//
+
+// 4. `function you create`
+ // 5. goes through your list of departments and sees whats checked
+ // 6. filters the EmployeeCollection
+ // 7. sets those results on the FilteredEmployeesCollection
+ // WIN.
+
+function formatTimes() {
+  _.each($("time[datetime]"), function(e) {
+  var $el = $(e); 
+  $el.text(moment($el.attr('datetime'))
+  .format("MMMM Do, YYYY")) });
+}
+
+$(function(){ 
 
   var employees = new EmployeeCollection(); //create collection
+  var filtered = new FilteredEmployeeCollection();//filtered employees
+
 
   employees.on("add", function(model) {
     var employeeView = new EmployeeView({model: model});
+
+
     $(".table-body").append(employeeView.render().el);
   });
 
-  employees.fetch().done(function(){
+ employees.fetch().done(function(){
 
     var firstModel = employees.first();
       var headings = firstModel.keys();
-      console.log(headings);
+      //console.log(headings);
       _.each(headings, function(heading) {
-         $("thead tr").append($("<th />").text(heading)); // appends heading in th
+         $("thead tr").append($("<th />").text(heading)); // appends heading in the employee view
       });
-  });
 
-  employees.filter()
+      formatTimes();
+
+
+      _.uniq(employees.map(function(e) { 
+        console.log(e.attributes.Dept);
+        return e.attributes.Dept; 
+        _.each()
+      }));
+  
+
+    // 1. map employee departments, uniq X
+   // 2. for each department, create the filter checkbox (probably its own template)
+  // 3. it'll handle an on change event that calls a `function you create`
+
+   // for each employee, 
+  // format the date
+     // set the FormattedStart attribute to the formatted date.
+
+     /*var CheckboxView = Backbone.View.extend({
+        template: JST["dept"],
+        tagname: "li",
+        var department = new EmployeeCollection();
+
+      getRoles: function() {
+        return _.uniq(this.department.pluck("dept"), false, function(dept){
+        return dept;
+        console.log(dept); 
+    });
+  }
+});*/
+
+  });  //end fetch
+
 });
+
 
 
--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,11 @@
 this["JST"] = this["JST"] || {};
+this["JST"]["dept"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<input=\"checkbox\" name=\"dept\">"
+    + escapeExpression(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"category","hash":{},"data":data}) : helper)))
+    + "</input>\n";
+},"useData":true});
+this["JST"] = this["JST"] || {};
 this["JST"]["employeeHead"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return "<th colspan=\"1\" data-name='key'>"
@@ -18,14 +25,9 @@ this["JST"]["employeeRow"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     + escapeExpression(((helper = (helper = helpers.Phone || (depth0 != null ? depth0.Phone : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Phone","hash":{},"data":data}) : helper)))
     + "</td>\n  <td>"
     + escapeExpression(((helper = (helper = helpers.Dept || (depth0 != null ? depth0.Dept : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Dept","hash":{},"data":data}) : helper)))
-    + "</td>\n  <td>"
+    + "</td>\n  <td class=\"start\"><time datetime=\""
     + escapeExpression(((helper = (helper = helpers.Start || (depth0 != null ? depth0.Start : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Start","hash":{},"data":data}) : helper)))
-    + "</td>\n";
-},"useData":true});
-this["JST"] = this["JST"] || {};
-this["JST"]["roles"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "<li><input type=\"checkbox\" data-name='role'>"
-    + escapeExpression(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"category","hash":{},"data":data}) : helper)))
-    + "</li>\n";
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.Start || (depth0 != null ? depth0.Start : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Start","hash":{},"data":data}) : helper)))
+    + "</time></td>\n";
 },"useData":true});
