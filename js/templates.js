8966aa48fdfd6c97ba8b0940d3c2a0d34a70b9bc
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-06 18:59:37 -0500

    completed base functionality - working on icons population

--- js/templates.js
+++ js/templates.js
@@ -27,6 +27,8 @@ this["JST"]["employeeRow"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     + escapeExpression(((helper = (helper = helpers.Phone || (depth0 != null ? depth0.Phone : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Phone","hash":{},"data":data}) : helper)))
     + "</td>\n  <td>"
     + escapeExpression(((helper = (helper = helpers.Dept || (depth0 != null ? depth0.Dept : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Dept","hash":{},"data":data}) : helper)))
+    + " "
+    + escapeExpression(((helper = (helper = helpers.Icon || (depth0 != null ? depth0.Icon : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Icon","hash":{},"data":data}) : helper)))
     + "</td>\n  <td class=\"start\"><time datetime=\""
     + escapeExpression(((helper = (helper = helpers.Start || (depth0 != null ? depth0.Start : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Start","hash":{},"data":data}) : helper)))
     + "\">"
